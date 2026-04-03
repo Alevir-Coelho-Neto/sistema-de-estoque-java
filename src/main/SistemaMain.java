d07c7a8f4ee3db71dd593a4a44dae01033a26062
Alevir Coelho Neto <alevircoelhoneto1@gmail.com>
2026-04-03 12:07:48 -0300

    feat: adiciona menu interativo no sistema de estoque

--- src/main/SistemaMain.java
+++ src/main/SistemaMain.java
@@ -14,19 +14,81 @@ public class SistemaMain {
 
         ProdutoService produtoService = new ProdutoService();
 
+        int opcao = 7;
+        String nomeProduto = null;
+        double valorProduto = 0;
+        int quantidadeProduto = 0;
+        String voltarMenuPrincipal = null;
 
-        produtoService.adicionarProdutos("Tablet", 1350, 15);
-        produtoService.adicionarProdutos("Notebook", 3000, 15);
-        produtoService.adicionarProdutos("Celular", 7500, 15);
-        produtoService.adicionarProdutos("Microfone",  500, 15);
-        produtoService.adicionarProdutos("Headset",  500, 15);
+        while (opcao != 0){
+            System.out.println("===== SISTEMA DE ESTOQUE =====");
+            System.out.println();
+            System.out.println("1 - Adicionar produto");
+            System.out.println("2 - Listar produtos");
+            System.out.println("3 - Buscar produto por ID");
+            System.out.println("4 - Buscar produto por nome");
+            System.out.println("5 - Remover produto por ID");
+            System.out.println("6 - Mostrar valor total do estoque");
+            System.out.println("7 - Mostrar valor por produto");
+            System.out.println("0 - Sair");
+            System.out.println();
+            System.out.print("Escolha uma opção: ");
+            opcao = sc.nextInt();
+            System.out.println();
+            sc.nextLine();
+
+            switch (opcao){
+                case 1:
+                    System.out.print("Digite o nome do produto: ");
+                    nomeProduto = sc.nextLine();
+                    System.out.print("Digite o valor do produto: ");
+                    valorProduto = sc.nextInt();
+                    System.out.print("Digite a quantidade: ");
+                    quantidadeProduto = sc.nextInt();
+                    sc.nextLine();
+
+                    produtoService.adicionarProdutos(nomeProduto, valorProduto, quantidadeProduto);
+                    System.out.println();
+                    System.out.println("Produto adicionado com sucesso!");
+                    System.out.println();
+
+                    break;
+                case 2:
+                    produtoService.listarProdutos();
+                    System.out.println();
+                    break;
+                case 3:
+                    System.out.println("...");
+                    break;
+                case 4:
+                    System.out.println("...");
+                    break;
+                case 5:
+                    System.out.println("...");
+                    break;
+                case 6:
+                    System.out.println("...");
+                    break;
+                case 7:
+                    System.out.println("...");
+                    break;
+                case 8:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Escolha uma opção valida!");
+                    break;
 
 
-       produtoService.listarProdutos();
-       produtoService.valorProdutosSeparadamente();
-       produtoService.valorTotalEstoque();
-       produtoService.buscaPorId(3);
-       produtoService.buscarPorNome("tableti");
+            }
+
+        }
+
+
+
+
+
+
 
 
         sc.close();
